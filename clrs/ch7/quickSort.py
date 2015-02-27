1a324574170e0eb3b9432d127c36a1f7f05baaa8
Compilers <compilers@compilers-vm.(none)>
2015-02-27 05:59:58 -0800

    20150227

--- clrs/ch7/quickSort.py
+++ clrs/ch7/quickSort.py
@@ -50,3 +50,45 @@ def partition(Array,left,right):
 			j -= 1
 	Array[pivot],Array[i-1] = Array[i-1],Array[pivot]
 	return i-1
+
+def partitionI(Array,left,right):
+	"""
+	>>> Array = [9,8,7,6,5,4,3,2,1]
+	>>> partitionI(Array,0,8)
+	0
+	>>> Array == [1,8,7,6,5,4,3,2,9]
+	True
+	>>> Array = [5,8,7,6,9,4,3,2,1]
+	>>> partitionI(Array,0,8)
+	0
+	>>> Array == [1,8,7,6,9,4,3,2,5]
+	True
+	>>> Array = [1,8,7,6,9,4,3,2,5]
+	>>> partitionI(Array,0,8)
+	4
+	>>> Array == [1,4,3,2,5,8,7,6,9]
+	True
+	>>> Array = [7,5]
+	>>> partitionI(Array,0,1)
+	0
+	>>> Array == [5,7]
+	True
+	>>> Array = [5,7]
+	>>> partitionI(Array,0,1)
+	1
+	>>> Array == [5,7]
+	True
+	>>> Array = [13,19,9,5,2,8,7,4,21,2,6,11]
+	>>> partitionI(Array,0,11)
+	8
+	>>> Array == [9,5,2,8,7,4,2,6,11,13,19,21]
+	True
+	"""	
+	pivot = right
+	i = left - 1
+	for j in range(left,right):
+		if Array[j] < Array[pivot]:
+			i += 1
+			Array[j],Array[i] = Array[i],Array[j]
+	Array[pivot],Array[i+1] = Array[i+1],Array[pivot]
+	return i+1
