eab7ad36ea406a633f226f513e6297d087ad56ae
zhuyoujun <zhuyojun0513@163.com>
2015-02-28 07:12:11 -0800

    add 20150228

--- sicp/cs61a/disc/disc04.py
+++ sicp/cs61a/disc/disc04.py
@@ -61,6 +61,13 @@ def closet_city(lat,lon,city1,city2):
     else:
         return get_name(city1)
 
+#########################
+#                       #
+#     fractions         #
+#                       #
+#########################
+
+
 from fractions import gcd
 def rational(n,d):
     """
@@ -150,9 +157,179 @@ def rational_pow(x, e):
     px,py = int(pow(nx,e)),int(pow(dx,e))
     return rational(px,py)
 
+def factorial(n):
+    """
+    >>> factorial(0)
+    1
+    >>> factorial(1)
+    1
+    >>> factorial(5)
+    120
+    """
+    if n == 0:
+        return 1 
+    else:
+        return n * factorial(n - 1)
+
+def approx_e(iter=100):
+    """
+    >>> approx_e()
+    2.718281828459045
+    >>> approx_e(10)
+    2.7182815255731922
+    >>> approx_e(0)
+    0.0
+    >>> approx_e(50)
+    2.718281828459045
+    """
+    e = rational(0,1)
+    for i in range(iter):
+        fact_i = rational(1,factorial(i))
+        e = add_rationals(e,fact_i)
+    return number(e)/denom(e)
+
+def inverse_rational(x):
+    """Returns the inverse of the given non-zero rational
+    number
+    >>> x = rational(2,3)
+    >>> inverse_rational(x) == rational(3,2)
+    True
+    >>> x = rational(1,3)
+    >>> inverse_rational(x) == rational(3,1)
+    True
+    """
+    nx, dx = number(x), denom(x)
+    assert nx != 0,"can no be zero."
+    return rational(dx,nx)
+
+def div_rationals(x, y):
+    """Returns x / y for given rational x and non-zero
+    rational y
+    >>> x = rational(2,3)
+    >>> y = rational(1,3)
+    >>> div_rationals(x, y) == rational(2,1)
+    True
+    """
+    assert number(x) !=0,"the second argument y can no be zero."
+    inve_y = inverse_rational(y)
+    return mul_rationals(x,inve_y)
 
 
 
+#########################
+def make_unit(catchphrase, damage):
+    """Each unit will have a string catchphrase 
+    and an integer amount of damage.
+    >>> make_unit('This is Jimmy.', 18)
+    ['This is Jimmy.', 18]
+    """
+    return [catchphrase,damage]
+
+def get_catchphrase(unit):
+    """Return the catchphrase element.
+    >>> unit = make_unit('This is Jimmy.', 18)
+    >>> get_catchphrase(unit)
+    'This is Jimmy.'
+    """
+    return unit[0]
+
+def get_damage(unit):
+    """Return the catchphrase element.
+    >>> unit = make_unit('This is Jimmy.', 18)
+    >>> get_damage(unit)
+    18
+    """
+    return unit[1]
+
+def battle(first, second):
+    """Simulates a battle between the first and second unit
+    >>> zealot = make_unit('My life for Aiur!', 16)
+    >>> zergling = make_unit('GRAAHHH!', 5)
+    >>> winner = battle(zergling, zealot)
+    GRAAHHH!
+    My life for Aiur!
+    >>> winner is zealot
+    True
+    """
+    first_catchphrase = get_catchphrase(first)
+    second_catchphrase = get_catchphrase(second)
+    first_damage = get_damage(first)
+    second_damage = get_damage(second)    
+    if first_damage < second_damage:
+        print(first_catchphrase)
+        print(second_catchphrase)
+        return second
+    else:
+        print(second_catchphrase)        
+        print(first_catchphrase)
+        return first
 
 
+def make_resource_bundle(minerals, gas):
+    """Return a function that represents a pair."""
+    def get(index):
+        if index == 0:
+            return minerals
+        else:
+            return gas
+    return get
+
+def select(bundle,i):
+    """Return the ith element in bundle."""
+    return bundle(i)
+
+def get_minerals(bundle):
+    """Return the 0th element in bundle."""
+    return select(bundle,0)
+
+def get_gas(bundle):
+    """Return the 1th element in bundle."""
+    return select(bundle,1)
+
+
+def make_building(unit, bundle):
+    def get(index):
+        if index == 0:
+            return unit
+        else:
+            return bundle
+    return get
+
+def select(building,i):
+    """Return the ith element in building."""
+    return building(i)
+
+def get_unit(building):
+    return select(building,0)
+
+def get_bundle(building):
+    return select(building,1)
+
+def build_unit(building, bundle):
+    """Constructs a unit if given the minimum amount of
+    resources.
+    Otherwise, prints an error message
+    >>> barracks = make_building(make_unit('Go go go!', 6),make_resource_bundle(50, 10))
+    >>> marine = build_unit(barracks, make_resource_bundle(20,20))
+    We require more minerals!
+    >>> marine = build_unit(barracks, make_resource_bundle(50,5))
+    We require more vespenegas!
+    >>> marine = build_unit(barracks, make_resource_bundle(40,5))
+    We require more minerals and vespenegas!
+    >>> marine = build_unit(barracks, make_resource_bundle(50,10))
+    >>> print(get_catchphrase(marine))
+    Go go go!
+    """
+    bundle_minerals   = get_minerals(bundle)
+    bundle_gas        = get_gas(bundle)
+    building_minerals = get_minerals(get_bundle(building))
+    building_gas      = get_gas(get_bundle(building))
+    if building_gas <= bundle_gas and building_minerals <= bundle_minerals:
+        return get_unit(building)
+    elif building_gas <= bundle_gas:
+        print("We require more minerals!")
+    elif building_minerals <= bundle_minerals:
+        print("We require more vespenegas!")
+    else:
+        print("We require more minerals and vespenegas!")
 
